ea7dc3323d1baddb6a729b470cc2361137d841bb
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 22:19:08 +0100

    modified:   0x02-ES6_classes/0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,20 +1,7 @@
-/**
- * Represents a classroom with a maximum number of students.
- *
- * @class ClassRoom
- */
 export default class ClassRoom {
-  /**
-     * Creates an instance of ClassRoom.
-     *
-     * @param {Number} maxStudentsSize - The maximum number of students the classroom can accommodate.
-     */
-  constructor (maxStudentsSize) {
-    /**
-       * The maximum number of students the classroom can accommodate.
-       * @type {Number}
-       * @private
-       */
-    this._maxStudentsSize = maxStudentsSize;
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
   }
-}
+
+  
