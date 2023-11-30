e4786f22c2d823a2fdf3fd1fda7ef84711385d70
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 22:32:08 +0100

    modified:   0x02-ES6_classes/5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,13 @@
 export default class Building {
-  constructor (sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+      if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+      this._sqft = sqft;
     }
-    this._sqft = sqft;
+  
+    get sqft() {
+      return this._sqft;
+    }
   }
-
-  get sqft () {
-    return this._sqft;
-  }
-
-  set sqft (sqft) {
-    this._sqft = sqft;
-  }
-}
+  
