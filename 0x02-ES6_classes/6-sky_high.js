dd71e045a35d92d38eb1ae42242b9d24ebebe577
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 22:29:50 +0100

    modified:   0x02-ES6_classes/6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,57 +1,20 @@
-// 6-sky_high.js
+import Building from './5-building';
 
-import Building from './5-building.js';
-
-/**
- * Represents a skyscraper building that extends from the Building class.
- *
- * @class SkyHighBuilding
- * @extends Building
- */
 export default class SkyHighBuilding extends Building {
-  /**
-   * Creates an instance of SkyHighBuilding.
-   *
-   * @param {Number} sqft - The square footage of the building.
-   * @param {Number} floors - The number of floors in the building.
-   */
-  constructor (sqft, floors) {
-    // Call the constructor of the parent class (Building)
+  constructor(sqft, floors) {
     super(sqft);
-
-    /**
-     * The number of floors in the building.
-     * @type {Number}
-     * @private
-     */
     this._floors = floors;
   }
 
-  /**
-   * Gets the square footage of the building.
-   *
-   * @returns {Number} The square footage of the building.
-   */
-  get sqft () {
+  get sqft() {
     return this._sqft;
   }
 
-  /**
-   * Gets the number of floors in the building.
-   *
-   * @returns {Number} The number of floors in the building.
-   */
-  get floors () {
+  get floors() {
     return this._floors;
   }
 
-  /**
-   * Overrides the evacuationWarningMessage method.
-   * Returns a string indicating the evacuation warning for the skyscraper.
-   *
-   * @returns {String} The evacuation warning message.
-   */
-  evacuationWarningMessage () {
-    return `Evacuate slowly the ${this._floors} floors`;
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
