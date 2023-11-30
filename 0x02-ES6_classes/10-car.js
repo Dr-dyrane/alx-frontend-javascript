8414688f14144243630d81fc9dad21a41960f08c
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 22:34:39 +0100

    modified:   0x02-ES6_classes/10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,48 +1,15 @@
-// 10-car.js
-
-/**
- * Represents a Car with brand, motor, and color attributes.
- *
- * @class Car
- */
 export default class Car {
-  /**
-     * Creates an instance of Car.
-     *
-     * @param {String} brand - The brand of the car.
-     * @param {String} motor - The type of motor of the car.
-     * @param {String} color - The color of the car.
-     */
-  constructor (brand, motor, color) {
-    /**
-       * The brand of the car.
-       * @type {String}
-       * @private
-       */
-    this._brand = brand;
-
-    /**
-       * The type of motor of the car.
-       * @type {String}
-       * @private
-       */
-    this._motor = motor;
-
-    /**
-       * The color of the car.
-       * @type {String}
-       * @private
-       */
-    this._color = color;
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    static get [Symbol.species]() {
+      return this;
+    }
+  
+    cloneCar() {
+      return new this.constructor[Symbol.species]();
+    }
   }
-
-  /**
-     * Creates and returns a new instance of the Car class.
-     *
-     * @method cloneCar
-     * @returns {Car} A new instance of the Car class.
-     */
-  cloneCar () {
-    return new Car(this._brand, this._motor, this._color);
-  }
-}
