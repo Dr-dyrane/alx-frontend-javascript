74f0a4b870b4edebb5395ba949d918ea51e72007
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 22:37:37 +0100

    modified:   0x02-ES6_classes/100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor (brand, motor, color, range) {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar () {
-    return super.cloneCar();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
