f97f6d84fffff00dbc6fe994f8c66e86f91a7325
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 23:04:56 +0100

    new file:   0x01-ES6_promise/100-await.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -5,4 +5,5 @@
  */
 export default function uploadPhoto (filename) {
   return Promise.reject(new Error(`${filename} cannot be processed`));
-}
+ }
+ 
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -10,18 +10,13 @@ import uploadPhoto from './5-photo-reject';
  * @returns {Promise<Array>} An array containing objects with status and value.
  */
 export default async function handleProfileSignup (firstName, lastName, fileName) {
-  try {
-    // Fetch user data and upload photo using async/await
-    const userData = await signUpUser(firstName, lastName);
-    const fileData = await uploadPhoto(fileName);
-
-    // Return a consistent array structure for fulfilled promises
-    return [
-      { status: 'fulfilled', value: userData },
-      { status: 'fulfilled', value: fileData }
-    ];
-  } catch (error) {
-    // Return an array structure for rejected promises
-    return [{ status: 'rejected', value: error.toString() }];
-  }
-}
+  return Promise
+  .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((response) => (
+    response.map((user) => ({
+      status: user.status,
+      value: user.status === 'fulfilled' ? user.value : String(user.reason),
+    }))
+  ));
+ }
+ 
