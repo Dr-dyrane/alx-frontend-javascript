4539fc88daf9cd1cc2bbbb5312799eb3440d7128
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 19:55:43 +0100

    modified:   0x02-ES6_classes/0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,9 +1,8 @@
 // 0-classroom.js
 
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      // Assign maxStudentsSize to the private attribute _maxStudentsSize
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor (maxStudentsSize) {
+    // Assign maxStudentsSize to the private attribute _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
   }
-  
+}
