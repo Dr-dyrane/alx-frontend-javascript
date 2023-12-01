7aeb4fe660551018152adc15d16c4fe163dc1bb3
Dr-dyrane <halodyrane@gmail.com>
2023-12-01 17:32:58 +0100

    add comments

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,31 @@
+/**
+ * Represents a Car object with brand, motor, and color properties.
+ */
 export default class Car {
+  /**
+   * Creates a new Car instance.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The type of motor of the car.
+   * @param {string} color - The color of the car.
+   */
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
+  /**
+   * Gets the species symbol for the class.
+   * @return {function} The class itself.
+   */
   static get [Symbol.species]() {
     return this;
   }
 
+  /**
+   * Clones the current car instance.
+   * @return {Car} A new instance of the same car class.
+   */
   cloneCar() {
     return new this.constructor[Symbol.species]();
   }
--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,28 @@
 import Car from './10-car';
 
+/**
+ * Represents an Electric Vehicle (EV) Car that extends the base Car class.
+ */
 export default class EVCar extends Car {
+  /**
+   * Creates a new EVCar instance.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The type of motor of the car.
+   * @param {string} color - The color of the car.
+   * @param {number} range - The range of the electric car in miles.
+   */
   constructor(brand, motor, color, range) {
+    // Call the constructor of the parent class (Car)
     super(brand, motor, color);
+
+    // Additional property specific to EVCar
     this._range = range;
   }
 
+  /**
+   * Gets the species symbol for the class, indicating the base class (Car).
+   * @return {function} The base Car class.
+   */
   static get [Symbol.species]() {
     return Car;
   }
